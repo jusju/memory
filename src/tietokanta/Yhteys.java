18e77c28e2b7dab2b020bb91312a6c0c868b0d1d
Juslin Jukka <jusju@haaga-helia.fi>
2019-02-21 17:30:47 +0200

    first commit

--- src/tietokanta/Yhteys.java
+++ src/tietokanta/Yhteys.java
@@ -1,5 +1,19 @@
 package tietokanta;
 
+import java.sql.*;
+
 public class Yhteys {
 
+	public static void main(String[] args) {
+		Connection c = null;
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:highscore.db");
+			System.out.println("SQLite DB connected");
+		} catch(Exception ex) {
+			
+		}
+	}
+	
 }
