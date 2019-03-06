6032bf2c4dc6156fe860c987e49ab28ecf3ae4d0
jusju <jusju@PAHM5016XP01.haagahelia.amk>
2019-03-06 08:47:32 +0200

    added db query

--- src/tietokanta/Yhteys.java
+++ src/tietokanta/Yhteys.java
@@ -6,14 +6,27 @@ public class Yhteys {
 
 	public static void main(String[] args) {
 		Connection c = null;
-		
+
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:highscore.db");
 			System.out.println("SQLite DB connected");
-		} catch(Exception ex) {
+			Statement stmt = c.createStatement();
+			String sql = "SELECT * FROM highscore";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				int score = rs.getInt("score");
+				String nickname = rs.getString("nickname");
+				System.out.println(score);
+				System.out.println(nickname);
+			}
+
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 }
