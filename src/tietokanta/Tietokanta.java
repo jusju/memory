d3f4b325805e9608993d8ecd8da10a31cc657aef
jusju <jusju@HHMX956.haagahelia.amk>
2019-03-10 12:55:07 +0200

    Tietokanta listaa pelaajat.

--- src/tietokanta/Tietokanta.java
+++ src/tietokanta/Tietokanta.java
@@ -1,37 +1,64 @@
 package tietokanta;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Pelaaja;
 
 public class Tietokanta {
 	private Connection connection = null;
-	
+	private Statement stmt = null;
+
 	public void luoYhteys() {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			connection = DriverManager.getConnection("jdbc:sqlite:highscore.db");
 			System.out.println("SQLite DB connected");
 			Statement stmt = connection.createStatement();
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	public void suljeYhteys() {
 		try {
+			stmt.close();
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void talleta(String nickname, int score) {
-		
+		luoYhteys();
+		try {
+			String sql = "INSERT INTO highscore(score, nickname) " + 
+					"VALUES(" + score + ",'" + nickname + "');";
+			stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
+
 	public List<Pelaaja> listaaPisteet() {
-		return null;
+		luoYhteys();
+		List<Pelaaja> tulokset = new ArrayList<Pelaaja>();
+		try {
+			String sql = "SELECT * FROM highscore";
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				int score = rs.getInt("score");
+				String nickname = rs.getString("nickname");
+				System.out.print(score + " ");
+				System.out.println(nickname);
+				tulokset.add(new Pelaaja(score, nickname));
+			}
+			stmt.execute(sql);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return tulokset;
 	}
 
 	public static void main(String[] args) {
